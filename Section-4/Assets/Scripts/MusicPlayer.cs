9ce58627c46c3a65dc317da663c9d085884c8844
Eduardo Alvarado <33001422+edualvarado@users.noreply.github.com>
2021-01-11 19:02:30 +0100

    ADDED: Section-4

---
+++ Section-4/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadFirstScene", 2f);
+    }
+
+    void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+
+    }
+}
